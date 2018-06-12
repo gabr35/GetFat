5462f5b5462628c3b0b77d720ab465fc4087601a
alidapar <alidapar@hotmail.com>
2018-06-12 16:44:54 +0200

    XD

--- app/src/main/java/com/example/bbarbg/getfat/MainActivity.java
+++ app/src/main/java/com/example/bbarbg/getfat/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.bbarbg.getfat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+
+import static java.lang.System.in;
 
 public class MainActivity extends AppCompatActivity {
-
+    String temp = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+
+        setTitle("Auf was hast du Lust?");
+
+        Spinner dropdown = findViewById(R.id.radius);
+        String[] items = new String[]{"Hier", "~300m entfernt", "Kanton"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+
     }
+
+
+    public void suchen(View v){
+
+        Intent intent = new Intent(getApplicationContext(), Restaurant.class);
+
+        boolean checked = ((CheckBox) v).isChecked();
+
+
+       switch(v.getId()) {
+            case R.id.cb_burger:
+                if (checked){
+                    intent.putExtra("burger", "burger");
+                    temp += "burger";
+                }
+            case R.id.cb_asian:
+                if (checked){
+                    intent.putExtra("asian", "asian");
+                }
+            case R.id.cb_doener:
+                if (checked){
+                    intent.putExtra("doener", "doener");
+                }
+            case R.id.cb_pizza:
+                if (checked){
+                    intent.putExtra("pizza", "pizza");
+                    temp += "pizza";
+                }
+            case R.id.cb_sandwich:
+                if (checked){
+                    intent.putExtra("sandwich", "sandwich");
+                }
+
+        }
+        this.setTitle(temp);
+
+
+
+
+
+        //startActivity(intent);
+
+    }
+
 }
--- app/src/main/java/com/example/bbarbg/getfat/Restaurant.java
+++ app/src/main/java/com/example/bbarbg/getfat/Restaurant.java
@@ -1,13 +1,65 @@
 package com.example.bbarbg.getfat;
 
+import android.app.ActionBar;
+import android.app.LoaderManager;
+import android.content.Intent;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
 
 public class Restaurant extends AppCompatActivity {
+    private String temp = "";
+    private ProgressBar progressBar;
+    private static final String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+
+        progressBar = findViewById(R.id.loading_restaurants_progress);
+        progressBar.setVisibility(View.VISIBLE);
+        progressBar.setX(184);
+
+        setTitle("Gefundene Restaurants");
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("burger");
+
+
+        progressBar = findViewById(R.id.loading_restaurants_progress);
+        progressBar.setVisibility(View.VISIBLE);
+        progressBar.setX(184);
+
+        try {
+            getRestaurants(url);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getRestaurants(String url) throws JSONException{
+        JSONObject jsonObj = new JSONObject(url);
+
+        //jsonObj.getString("temp");
+
+
+
     }
 }
